8ed96448bfecf37057bf8e82a71fb13791a1f65d
Ferran Basora <fcsonline@gmail.com>
2024-07-01 16:33:42 +0200

    New eslint check for valid deps

---
+++ rules/no-valid-dependencies.js
@@ -0,0 +1,61 @@
+const exceptions = ['navigation.ts', 'sidebar.ts', 'events.ts']
+
+// @ts-check
+/** @type {import('eslint').Rule.RuleModule} */
+module.exports = {
+  create(context) {
+    return {
+      ImportDeclaration: function (node) {
+        const fileName = context.getFilename()
+        const moduleRegex = /modules\/(?<module>[^\/]+)/
+        const dsRegex = /design-system\//
+
+        const value = node.source.value
+
+        const matchFileName = fileName.match(moduleRegex)
+        const matchNode = value.match(moduleRegex)
+
+        const specialFile = (fileName) => {
+          return exceptions.some((exception) => fileName.endsWith(exception))
+        }
+
+        if (matchFileName && matchNode) {
+          const { module: moduleFileName } = matchFileName.groups
+          const { module: moduleNode } = matchNode.groups
+
+          // Valid depedency, same module
+          if (moduleFileName === moduleNode) return
+
+          // Invalid dependency: Core must to have dependencies to other modules
+          if (moduleFileName === 'core' && !specialFile(moduleNode)) {
+            context.report({
+              node,
+              message:
+                'Core must not have dependencies to other modules',
+            })
+          }
+
+          // TODO: We need to check that special files don't import extra stuff from other modules
+
+          if (moduleNode !== 'core') {
+            // Invalid dependency: Module one is coupled to module two
+            context.report({
+              node,
+              message:
+                `'${moduleFileName}' module is coupled to '${moduleNode}' module. This is not allowed`
+            })
+          }
+        }
+
+        // Invalid dependency: Design System must to have dependencies to other modules
+        if (fileName.match(dsRegex) && matchNode) {
+          context.report({
+            node,
+            message:
+            `Design System must not be coupled to application modules such as ${matchNode.groups.module}`,
+          })
+        }
+      },
+    }
+  },
+}
