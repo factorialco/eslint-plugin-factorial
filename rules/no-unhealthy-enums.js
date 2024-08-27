c8e03ee87e318fa1dcf3fab39443f1847a18ebfc
Ferran Basora <fcsonline@gmail.com>
2024-08-27 08:54:28 +0200

    Handle more cases in unhealthy enums

--- rules/no-unhealthy-enums.js
+++ rules/no-unhealthy-enums.js
@@ -3,7 +3,9 @@ module.exports = {
     return {
       TSEnumDeclaration: function (node) {
         const allEqual = node.members.reduce((memo, item) => {
+          if (!item.initializer) return true
           if (!item.initializer.type.includes('Literal')) return false
+          if (item.id.type !== 'Identifier') return false
 
           return memo && item.id.name.toLowerCase() === item.initializer.value.toLowerCase()
         }, true)
